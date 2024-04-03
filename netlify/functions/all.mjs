ddf9c0ca96967fb7135f9e973fa9cf91fafc6959
Niels Hoppe <mail@nielshoppe.de>
2024-04-03 15:31:19 +0200

    Steps 1-3: Load data, create JSON, return response

--- netlify/functions/all.mjs
+++ netlify/functions/all.mjs
@@ -1,6 +1,19 @@
-export default async (request, context) => {
-    
-    return new Response("Hello, World!");
+export default async (req, context) => {
+
+    // Step 1: Load countries
+    const countries = require('../../countries.json')
+
+    // Step 2: Create countries JSON
+    const result = countries
+
+    const body = JSON.stringify(result)
+
+    // Step 3: Return JSON response
+    return new Response(body, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 };
 
 export const config = {
