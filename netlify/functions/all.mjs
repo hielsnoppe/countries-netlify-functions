3a7b73f23864142878132165a1c46864b528be92
Niels Hoppe <mail@nielshoppe.de>
2024-04-03 15:33:07 +0200

    Steps 7-10: Implement function `filterKeys`

--- netlify/functions/all.mjs
+++ netlify/functions/all.mjs
@@ -1,17 +1,32 @@
-// Step 4: Define filterKeys function
+/**
+ * Heads up! Will error when key is not in object.
+ * 
+ * @param {*} obj 
+ * @param {*} keys 
+ * @returns 
+ */
 const filterKeys = (obj, keys) => {
     
-    return obj // TODO
+    // Step 7: Create empty object
+    const result = {}
+
+    // Step 8: Iterate over keys
+    for (let key of keys) {
+
+        // Step 9: Copy value for each key to result
+        result[key] = obj[key]
+    }
+
+    // Step 10: Return result
+    return result
 }
 
 export default async (req, context) => {
 
     const countries = require('../../countries.json')
 
-    // Step 5: Define keys constant
-    const keys = [] // TODO
+    const keys = ['name'] // TODO
 
-    // Step 6: Filter countries by fields
     const result = countries.map((country) => {
         return filterKeys(country, keys)
     })
